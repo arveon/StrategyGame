5a1b94d0805f33b17420741a715545ab02ee73b6
AleksejsLoginovs <aloginovs@dundee.ac.uk>
2017-07-23 15:43:21 +0100

    refined saved files search so it only looks for .save files. Removed .save from displayed names.

--- SDL_Framework/FileHandler.h
+++ SDL_Framework/FileHandler.h
@@ -11,11 +11,6 @@
 ///Class contains all methods for file handling required in the program
 class file_handler
 {
-private:
-	static std::string wchar_to_str(wchar_t* wchar);
-	static const wchar_t* str_to_wchar(std::string str);
-	
-
 public:
 	static std::vector<list_item> get_saved_files();
 	file_handler();
