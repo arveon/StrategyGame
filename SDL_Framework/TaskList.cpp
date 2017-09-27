36e8d902cffbf816d89e97ff45e444cba9267e6c
Aleksejs Loginovs (Student) <aloginovs@dundee.ac.uk>
2017-09-27 12:35:58 +0100

    Added tasks to task list.

--- SDL_Framework/TaskList.cpp
+++ SDL_Framework/TaskList.cpp
@@ -1,5 +1,7 @@
-//TODO: get rid of float int conversion warnings
+//TODO: decide on tile size (maybe need to use multiplier)
+//TODO: draw simple tiles for grass, mud, road, water, rocks
+//TODO: create a bigger map
 //TODO: create a player object
 //TODO: allow player move in game state
-//TODO: character creation screen
+//TODO: character creation screen (not sure if needed yet)
 //TODO: create a sample cutscene
