09d8aa67ed35ef2f482d04683b4979dfca9521b6
AleksejsLoginovs <arveonfin@gmail.com>
2017-10-12 09:07:55 +0100

    Added tasks. Moved get_tiletype_at() method into a .cpp file

--- SDL_Framework/TaskList.cpp
+++ SDL_Framework/TaskList.cpp
@@ -1,7 +1,8 @@
-//TODO: redo texture loading. load tilesheet once, cut out whatever is needed and discard it
+
+//TODO: COMMENT THE CODE
+//TODO: make mouse hover recognise player objects as well
+	//TODO: change map_manager::map_entities and items to arrays with NULL if nothing is there
 //TODO: do the secondary ESC menu
-//TODO: create a bigger map
-//TODO: create a player object
 //TODO: allow player move in game state
 //TODO: character creation screen (not sure if needed yet)
 //TODO: create a sample cutscene
